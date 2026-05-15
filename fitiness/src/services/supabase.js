2c5bbf05b9d78b65094feb52bbe493b1fa94e4f0
✧ Sophia Cordeiro✧ <emilisophiassss@gmail.com>
2026-05-15 16:57:19 +0000

    .

--- fitiness/src/services/supabase.js
+++ fitiness/src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'SUA_URL';
-const supabaseAnonKey = 'SUA_CHAVE';
+const supabaseUrl = 'https://xejbpqhbookzgvaqzpdd.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhlamJwcWhib29remd2YXF6cGRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NDQ5MDUsImV4cCI6MjA5NDMyMDkwNX0.hQhVFIyO8bf5uPO20chVf_5kr_nRfUzGYx9ufo0eiuk';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
